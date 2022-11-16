d3be46f38e147b20006c7bf839caa8a91422d9ae
kalaithuli <kalaisiva4132@gmail.com>
2022-11-16 19:39:53 +0530

    First add

---
+++ Prog1_Arithmetic_Operations.py
@@ -0,0 +1,12 @@
+# WAP to do all four arithmetic operations.
+a= float(input("Enter a number "))
+b= float(input("Enter a number "))
+sum = a+b
+diff = a-b
+prod = a*b
+quo = a/b
+print("Sum = ", sum)
+print(" Difference = ", diff)
+print("Product = ", prod)
+print(" Quotient " , quo)
+
